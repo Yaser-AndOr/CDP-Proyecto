fffc3f0796bae93a05ce4145186ea4090ca716e6
Yaser Andrade <77845161+Yaser-AndOr@users.noreply.github.com>
2022-12-05 11:26:55 -0600

    Simulador v2.0

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 let rpm = 0;
 let mate = new Object();
-const   m1= 0.004,
-        m2= 0.002,
-        m3= 0.005,
-        m4= 0.119,
-        m5= 0.049,
-        m6= 0.001;
+mate.m = [0,0,0,0,0,0,0];
+mate.e = [0,0,0,0,0,0,0];
+mate.ma = [0,0,0,0,0,0,0];
+let   m =[ 0.004,0.002,0.005,0.119,0.049,0.001]
         r11= 0.008,
         r21= 0.0015,
         r31= 0.005,
@@ -28,54 +26,49 @@ function velocidad(old){
 }
 
 function wrvAng(){
-    let text = `RPM: ${rpm}<p>Velocidad Angular: ${Math.trunc(mate.vang)}`
+    let text = `RPM: ${rpm}<p>Velocidad Angular: ${Math.trunc(mate.vang)} Rad/s<p>Desplazamiento angular: ${Math.trunc(mate.w)} Kg*m^2/s`
     document.getElementById("vangular").innerHTML = text;
 }
 function wrMI(){
-    let text = `Esmeril: ${mate.mie} Kgm^2<p>Eje Esmeril: ${mate.miee} Kgm^2
-    <p>Sujetador: ${mate.mis} Kgm^2<p>Eje Central: ${mate.miec} Kgm^2
-    <p>Bobina: ${mate.mib} Kgm^2<p>Masas: ${mate.miem} Kgm^2<p>Sistema: ${mate.miss} Kgm^2`
+    let text = `Esmeril: ${mate.m[0]} Kgm^2<p>Eje Esmeril: ${mate.m[1]} Kgm^2
+    <p>Sujetador: ${mate.m[2]} Kgm^2<p>Eje Central: ${mate.m[3]} Kgm^2
+    <p>Bobina: ${mate.m[4]} Kgm^2<p>Masas: ${mate.m[5]} Kgm^2<p>Sistema: ${mate.m[6]} Kgm^2`
     document.getElementById("inercia").innerHTML = text;
 }
 function wrECR(){
-    let text = `Esmeril: ${mate.ecre} J<p>Eje Esmeril: ${mate.ecree} J
-    <p>Sujetador: ${mate.ecrs} J<p>Eje Central: ${mate.ecrec} J
-    <p>Bobina: ${mate.ecrb} J<p>Masas: ${mate.ecrem} J<p>Sistema: ${mate.ecrss} J`
+    let text = `Esmeril: ${mate.e[0]} J<p>Eje Esmeril: ${mate.e[1]} J
+    <p>Sujetador: ${mate.e[2]} J<p>Eje Central: ${mate.e[3]} J
+    <p>Bobina: ${mate.e[4]} J<p>Masas: ${mate.e[5]} J<p>Sistema: ${mate.e[6]} J`
     document.getElementById("cinetica").innerHTML = text;
 }
 function wrMA(){
-    let text = `Esmeril: ${mate.mae} Kgm^2/s<p>Eje Esmeril: ${mate.maee} Kgm^2/s
-    <p>Sujetador: ${mate.mas} Kgm^2/s<p>Eje Central: ${mate.maec} Kgm^2/s
-    <p>Bobina: ${mate.mab} Kgm^2/s<p>Masas: ${mate.maem} Kgm^2/s<p>Sistema: ${mate.mass} Kgm^2/s`
+    let text = `Esmeril: ${mate.ma[0]} Kgm^2/s<p>Eje Esmeril: ${mate.ma[1]} Kgm^2/s
+    <p>Sujetador: ${mate.ma[2]} Kgm^2/s<p>Eje Central: ${mate.ma[3]} Kgm^2/s
+    <p>Bobina: ${mate.ma[4]} Kgm^2/s<p>Masas: ${mate.ma[5]} Kgm^2/s<p>Sistema: ${mate.ma[6]} Kgm^2/s`
     document.getElementById("momento").innerHTML = text;
 }
 
 function calculos(){
     mate.vang = 2*Math.PI*rpm/60;
+    mate.w = Math.sqrt(2*mate.vang*(rpm*2*3.1416));
     wrvAng();
-    mate.mie = m1*(Math.pow(r11,2))/2;
-    mate.miee = m2*(Math.pow(r21,2))/2;
-    mate.mis = m3*(Math.pow(r31,2)+Math.pow(r32,2))/2;
-    mate.miec = m4*(Math.pow(r41,2))/2;
-    mate.mib = m5*(Math.pow(r51,2)+Math.pow(r52,2))/2;
-    mate.miem = 8*m6*(Math.pow(r61,2));
-    mate.miss = mate.mie+mate.miee+mate.mis+mate.miec+mate.mib+mate.miem;
+    mate.m[0] = m[0]*(Math.pow(r11,2))/2;
+    mate.m[1] = m[1]*(Math.pow(r21,2))/2;
+    mate.m[2] = m[2]*(Math.pow(r31,2)+Math.pow(r32,2))/2;
+    mate.m[3] = m[3]*(Math.pow(r41,2))/2;
+    mate.m[4] = m[4]*(Math.pow(r51,2)+Math.pow(r52,2))/2;
+    mate.m[5] = 8*m[5]*(Math.pow(r61,2));
+    mate.m[6] = mate.m[0]+mate.m[1]+mate.m[2]+mate.m[3]+mate.m[4]+mate.m[5];
     wrMI();
-    mate.ecre = mate.mie*(Math.pow(mate.vang,2))/2;
-    mate.ecree = mate.miee*(Math.pow(mate.vang,2))/2;
-    mate.ecrs = mate.mis*(Math.pow(mate.vang,2))/2;
-    mate.ecrec = mate.miec*(Math.pow(mate.vang,2))/2;
-    mate.ecrb = mate.mib*(Math.pow(mate.vang,2))/2;
-    mate.ecrem = mate.miem*(Math.pow(mate.vang,2))/2;
-    mate.ecrss = mate.ecre+mate.ecree+mate.ecrs+mate.ecrec+mate.ecrb+mate.ecrem;
+    for (let x = 0; x < 6; x++) {
+        mate.e[x] = mate.m[x]*(Math.pow(mate.vang,2))/2;   
+    }
+    mate.e[6] = mate.e[0]+mate.e[1]+mate.e[2]+mate.e[3]+mate.e[4]+mate.e[5];
     wrECR();
-    mate.mae = mate.mie*mate.vang;
-    mate.maee = mate.miee*mate.vang;
-    mate.mas = mate.mis*mate.vang;
-    mate.maec = mate.miec*mate.vang;
-    mate.mab = mate.mib*mate.vang;
-    mate.maem = mate.miem*mate.vang;
-    mate.mass = mate.mae+mate.maee+mate.mas+mate.maec+mate.mab+mate.maem;
+    for (let x = 0; x < 6; x++) {
+        mate.ma[x] = mate.m[x]*mate.vang;
+    }
+    mate.ma[6] = mate.ma[0]+mate.ma[1]+mate.ma[2]+mate.ma[3]+mate.ma[4]+mate.ma[5];
     wrMA();
 }
 
@@ -88,13 +81,7 @@ function off(){
     document.getElementById("rotador4").style.animation = `rotores ${rpm}s linear infinite`;
     document.getElementById("rotador5").style.animation = `rotores ${rpm}s linear infinite`;
     document.getElementById("rotador6").style.animation = `rotores ${rpm}s linear infinite`;
-    mate.mie = 0;
-    mate.miee = 0;
-    mate.mis = 0;
-    mate.miec = 0;
-    mate.mib = 0;
-    mate.miem = 0;
-    mate.miss = mate.mie+mate.miee+mate.mis+mate.miec+mate.mib+mate.miem;
+    mate.m = [0,0,0,0,0,0,0];
     wrMI();
 }
 function v1(){
